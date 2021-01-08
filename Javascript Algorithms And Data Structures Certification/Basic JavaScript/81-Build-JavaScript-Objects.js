dca2cfd081b31a45e9ee6156581104d030bb48f4
Yannick Severo <yannicksevero@gmail.com>
2021-01-08 20:09:50 +0100

    Add Basic Javascript solutions

---
+++ Javascript Algorithms And Data Structures Certification/Basic JavaScript/81-Build-JavaScript-Objects.js
@@ -0,0 +1,9 @@
+var myDog = {
+  // Only change code below this line
+      "name": "Snoopy",
+      "legs": 4,
+      "tails": 1,
+      "friends": ["cats", "birds"]
+  
+  // Only change code above this line
+  };
