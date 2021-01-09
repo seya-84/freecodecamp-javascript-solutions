fa39cbe10eed647416f30d1bc18862454bc331ef
Yannick Severo <yannicksevero@gmail.com>
2021-01-09 07:47:02 +0100

    Add Basic Javascript solutions

--- Javascript Algorithms And Data Structures Certification/Basic JavaScript/84-Accessing-Object-Properties-with-Variables.js
+++ Javascript Algorithms And Data Structures Certification/Basic JavaScript/84-Accessing-Object-Properties-with-Variables.js
@@ -7,5 +7,5 @@ var testObj = {
 
 // Only change code below this line
 
-var playerNumber = 16;       // Change this line
-var player = testObj[playerNumber];   // Change this line
+var playerNumber = 16;// Change this line
+var player = testObj[playerNumber];// Change this line
