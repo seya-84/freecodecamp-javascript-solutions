91c62484c6e54dfce55df63bb7d3ae7c27fd6cc3
Yannick Severo <yannicksevero@gmail.com>
2021-01-09 08:49:27 +0100

    Add Basic Javascript solutions

---
+++ Javascript Algorithms And Data Structures Certification/Basic JavaScript/90-Manipulating-Complex-Objects.js
@@ -0,0 +1,26 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Snoopy",
+    "title": "Bow-Bow",
+    "release_year": 2021,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+  },
+
+  // Add a record here
+];
