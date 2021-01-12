c393e68780e2adeeeb4362c75622e044329ce050
Yannick Severo <yannicksevero@gmail.com>
2021-01-12 20:11:11 +0100

    Add-ES6-Solutions

--- Javascript Algorithms And Data Structures Certification/ES6/17-Create-Strings-using-Template-Literals.js
+++ Javascript Algorithms And Data Structures Certification/ES6/17-Create-Strings-using-Template-Literals.js
@@ -7,7 +7,7 @@ function makeList(arr) {
   // Only change code below this line
   const failureItems = [];
   for(let i = 0; i < arr.length; i++) {
-    failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
   }
   // Only change code above this line
 
