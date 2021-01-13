1828f063edba187a4672c359383f6aa2c19066e3
Yannick Severo <yannicksevero@gmail.com>
2021-01-13 14:03:23 +0100

    Add-ES6-Solutions

---
+++ Javascript Algorithms And Data Structures Certification/ES6/23-Use-export-to-Share-a-Code-Block.js
@@ -0,0 +1,19 @@
+// Solution 1
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString};
+
+// Solution 2
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+export const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
