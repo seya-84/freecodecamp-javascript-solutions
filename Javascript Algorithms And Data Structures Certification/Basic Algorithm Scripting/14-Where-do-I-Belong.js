283627ec7a176c185a89a7311811ff41c340ff84
Yannick Severo <yannicksevero@gmail.com>
2021-01-31 09:01:00 +0100

    Add Basic Algoritm Scripting Solutions

---
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/14-Where-do-I-Belong.js
@@ -0,0 +1,14 @@
+function getIndexToIns(arr, num) {
+  
+  let times = arr.length; // 2
+  let count = 0;
+
+  for(let i = 0; i < times; i++) {
+    if(num > arr[i]) {
+      count++;
+    }
+  } 
+  return count; 
+}
+
+getIndexToIns([40, 60], 50);
