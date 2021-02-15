cace178a762be64f9c32e8fcaf310f3331b08c79
Yannick Severo <yannicksevero@gmail.com>
2021-02-15 11:18:51 +0100

    Add Intermediate Algorithm Scripting Solutions

---
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/04-Wherefore-art-thou.js
@@ -0,0 +1,26 @@
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  // Only change code below this line
+  return collection.filter(function(obj) {
+    for (var i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+// Only change code above this line
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
