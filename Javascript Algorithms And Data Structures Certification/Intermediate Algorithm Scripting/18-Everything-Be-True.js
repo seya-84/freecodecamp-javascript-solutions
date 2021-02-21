034308ee55575d0d5460f4d97bebe87db546e557
Yannick Severo <yannicksevero@gmail.com>
2021-02-21 15:20:11 +0100

    Add Intermediate Algorithm Scripting Solutions

---
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/18-Everything-Be-True.js
@@ -0,0 +1,19 @@
+function truthCheck(collection, pre) {
+  let counter = 0;
+  for (var c in collection) {
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  return counter == collection.length;
+}
+
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" },
+  ],
+  "sex"
+);
