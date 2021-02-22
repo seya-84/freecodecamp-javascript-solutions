99b8c384295ac37906451d3f62146dad5738310a
Yannick Severo <yannicksevero@gmail.com>
2021-02-22 17:12:41 +0100

    Add Intermediate Algorithm Scripting Solutions

---
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/19-Arguments-Optional.js
@@ -0,0 +1,29 @@
+function addTogether() {
+  let checkNum = function(num) {
+    if (typeof num !== "number") {
+      return undefined;
+    } else return num;
+  };
+
+  if (arguments.length > 1) {
+    let a = checkNum(arguments[0]);
+    let b = checkNum(arguments[1]);
+    if (a === undefined || b === undefined) {
+      return undefined;
+    } else {
+      return a + b;
+    }
+  } else {
+    let c = arguments[0];
+
+    if (checkNum(c)) {
+      return function(arg2) {
+        if (c === undefined || checkNum(arg2) === undefined) {
+          return undefined;
+        } else {
+          return c + arg2;
+        }
+      };
+    }
+  }
+}
